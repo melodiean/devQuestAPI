954282e38976b50bf7940f48c95005d14cc94f05
Melodiean <melodynsita@gmail.com>
2020-11-30 22:17:39 +0300

    Refactor

--- app.js
+++ app.js
@@ -1,50 +1,76 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userRouter = require('./routes/users')
+// const {auth} = require('./controllers/auth')
+const db = require("./config/config").get(process.env.NODE_ENV);
 
-const mongoose = require('mongoose')
+const app = express();
 const dotenv = require('dotenv') 
 
 dotenv.config()
 
-const port = process.env.apiPort
+// app use
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cookieParser());
 
-const apiDb = process.env.db;
-
-mongoose.connect(apiDb, {
+// database connection
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb://localhost/group4",
+  {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify: false, 
     useCreateIndex: true
-})
+},
+  function (err) {
+    if (err) console.log(err);
+    console.log("database is connected");
+  }
+);
 
-var db = mongoose.connection
+// var db = mongoose.connection
 
-db.on('error', console.error.bind(console,'Connection Failed: '));
+// db.on('error', console.error.bind(console,'Connection Failed: '));
 
-db.once('open',(err)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log('Database Connected!'); 
-});
+// db.once('open',(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('Database Connected!'); 
+// });
+
+
+
+// app.get("/", function (req, res) {
+//   res.status(200).send(`Welcome to login , sign-up api`);
+// });
 
 app.get('/',(req,res)=>{
-    if(err) {
-        console.log(err);
-    }
-    else{
-    res.json({message:'From Edu Api!'});
-    }
+  if(err) {
+      console.log(err);
+  }
+  else{
+  res.json({message:'From Edu Api!'});
+  }
 
 });
 
-app.listen(port,()=>{
-    try{
-        console.log(`Api is running at http://localhost:${port}/`); 
-    }
-    catch(err){
-        console.log(
-        `Error: ${err.message}`
-        );
-    }
-})
+app.use('/', userRouter)
+
+// listening port
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`app is live at ${PORT}`);
+//   try{
+//     console.log(`Api is running at http://localhost:${port}/`); 
+// }
+// catch(err){
+//     console.log(
+//     `Error: ${err.message}`
+//     );
+// }
+});
---
+++ controllers/user.controller.js
@@ -0,0 +1,83 @@
+const User = require("../models/user.model");
+
+// register user
+exports.register_user = function (req, res) {
+  // taking a user
+  const newuser = new User(req.body);
+
+  if (newuser.password != newuser.password2)
+    return res.status(400).json({ message: "password not match" });
+
+  User.findOne({ email: newuser.email }, function (err, user) {
+    if (user)
+      return res.status(400).json({ auth: false, message: "email exits" });
+
+    newuser.save((err, doc) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ success: false });
+      }
+      res.status(200).json({
+        succes: true,
+        user: doc,
+      });
+    });
+  });
+};
+
+// log user in
+exports.login_user = function (req, res) {
+  let token = req.cookies.auth;
+  User.findByToken(token, (err, user) => {
+    if (err) return res(err);
+    if (user)
+      return res.status(400).json({
+        error: true,
+        message: "You are already logged in",
+      });
+    else {
+      User.findOne({ email: req.body.email }, function (err, user) {
+        if (!user)
+          return res.json({
+            isAuth: false,
+            message: " Auth failed ,email not found",
+          });
+
+        user.comparepassword(req.body.password, (err, isMatch) => {
+          if (!isMatch)
+            return res.json({
+              isAuth: false,
+              message: "password doesn't match",
+            });
+
+          user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie("auth", user.token).json({
+              isAuth: true,
+              id: user._id,
+              email: user.email,
+            });
+          });
+        });
+      });
+    }
+  });
+};
+
+// get user profile
+exports.profile = function (req, res) {
+  res.json({
+    isAuth: true,
+    id: req.user._id,
+    email: req.user.email,
+    name: `${req.user.firstname} ${req.user.lastname}`,
+  });
+};
+
+// log user out
+exports.logout = function (req, res) {
+  req.user.deleteToken(req.token, (err, user) => {
+    if (err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
+};
