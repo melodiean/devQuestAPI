72a504b09042e498fe09b4c9e88cdc6d088475e6
Melodiean <melodynsita@gmail.com>
2020-11-30 22:24:55 +0300

    Refactor dev files

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const {auth} = require('../controllers/auth');
+const { register_user,login_user, profile, logout } = require('../controllers/user.controller')
+
+// adding new user (sign-up route)
+router.post('/auth/register', register_user);
+
+ // login user
+router.post('/auth/login', login_user);
+
+// get logged in user
+router.get('/auth/profile',auth, profile);
+
+//logout user
+router.get('/auth/logout',auth,logout); 
+
+module.exports = router
