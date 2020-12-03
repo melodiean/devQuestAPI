1af792b3edb215fcb21d6d591ae3584a1894ab14
Melodiean <melodynsita@gmail.com>
2020-12-03 16:16:41 +0300

    Edit env

--- models/user.model.js
+++ models/user.model.js
@@ -66,7 +66,7 @@ userSchema.methods.comparepassword = function (password, cb) {
 
 userSchema.methods.generateToken = function (cb) {
   var user = this;
-  var token = jwt.sign(user._id.toHexString(), config.SECRET);
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
 
   user.token = token;
   user.save(function (err, user) {
