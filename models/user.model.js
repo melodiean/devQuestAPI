0ba9b6ee47dd330db971bf1f0fa4b60ae1d55f91
Melodiean <melodynsita@gmail.com>
2020-12-03 16:34:56 +0300

    Edit confiq

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const confiq = require("../config/config").get(process.env.NODE_ENV);
+// const process.env.SECRET = require("../config/config").get(process.env.NODE_ENV);
 const salt = 10;
 
 var mongoose = require("mongoose");
@@ -79,7 +79,7 @@ userSchema.methods.generateToken = function (cb) {
 userSchema.statics.findByToken = function (token, cb) {
   var user = this;
 
-  jwt.verify(token, confiq.SECRET, function (err, decode) {
+  jwt.verify(token, process.env.SECRET, function (err, decode) {
     user.findOne({ _id: decode, token: token }, function (err, user) {
       if (err) return cb(err);
       cb(null, user);
