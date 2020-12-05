5df5d7167c475beef3db90ae8343e251a955b7a3
Melodiean <melodynsita@gmail.com>
2020-12-05 14:51:36 +0300

    Remove whitespace, format

--- models/questions.model.js
+++ models/questions.model.js
@@ -15,38 +15,38 @@ const questionSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
-    answer_info:[{
-        answer: String,
-        createdBy: String  // user that posted the answer
-        ,
-        comments: [{
-            comment: String,
-            createdBy: String, // indicates user who posted the comment
-            // required: false
-        }],
-        vote:{
-            upvote:{
-                type: Boolean,
-                // default: false,
-                required: false
-            },
-            downvote:{
-                type: Boolean,
-                // default: false,
-                required: false
-            }
+  answer_info: [
+    {
+      answer: String,
+      createdBy: String, // user that posted the answer
+      comments: [
+        {
+          comment: String,
+          createdBy: String, // indicates user who posted the comment
+          // required: false
         },
-        // here an answer can be marked as the preferred one
-        best_answer: {
-            type: Boolean,
-            // default:false,
-            required: false
-        }
-    }
-
-]
-}
-);
+      ],
+      vote: {
+        upvote: {
+          type: Boolean,
+          // default: false,
+          required: false,
+        },
+        downvote: {
+          type: Boolean,
+          // default: false,
+          required: false,
+        },
+      },
+      // here an answer can be marked as the preferred one
+      best_answer: {
+        type: Boolean,
+        // default:false,
+        required: false,
+      },
+    },
+  ]
+});
 
 // Question model to be exported
 const Question = mongoose.model("Question", questionSchema);
