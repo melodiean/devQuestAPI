14320d7f2152afb0c00d05b743d861562f455c3b
Melodiean <melodynsita@gmail.com>
2021-02-25 22:39:26 +0300

    Edits

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const questionsRouter = require("./routes/questions");
 dotenv.config();
 
 const db = process.env.apiDb;
-//  ||
-// process.env.lDb
 
 // app use
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -45,7 +43,7 @@ app.use("/api/v1", userRouter);
 app.use("/api/v1", questionsRouter);
 
 // listening port
-const PORT = process.env.PORT || process.env.apiPort;
+const PORT = process.env.apiPort;
 app.listen(PORT, () => {
   console.log(`API is live at http://localhost:${PORT}`);
 });
